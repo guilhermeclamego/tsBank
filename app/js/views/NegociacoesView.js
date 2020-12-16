e16daaa339b20301f6f2be0e035bafb0cf006cbf
guilhermeclamego <guilhermecesarl20@gmail.com>
2020-12-16 16:49:31 -0300

    Elaborando solução para view

---
+++ app/js/views/NegociacoesView.js
@@ -0,0 +1,40 @@
+class NegociacoesView {
+    constructor(seletor) {
+        this._elemento = document.querySelector(seletor);
+    }
+    //Converte o template ao elemento do DOM
+    update(model) {
+        this._elemento.innerHTML = this.template(model);
+    }
+    //Define o que vai ser apresentado ao usuário
+    template(model) {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+
+            <tbody>
+            </tbody>
+                ${model.paraArray().map(negociacao => {
+            return `
+                        <tr>
+                            <td>${negociacao.data.getDate()}/${negociacao.data.getMonth() + 1}/${negociacao.data.getFullYear()}</td>
+                            <td>${negociacao.quantidade}</td>
+                            <td>${negociacao.valor}</td>
+                            <td>${negociacao.volume}</td>
+                        </tr>
+                    `;
+        }).join('')}
+            <tfoot>
+            </tfoot>
+        </table>         
+        
+    `;
+    }
+}
--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,11 +3,14 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
         this._inputData = <HTMLInputElement> document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement> document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement> document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event){        
@@ -18,7 +21,10 @@ class NegociacaoController {
             parseFloat(this._inputValor.value)
         );
 
-        console.log(negociacao.quantidade);
+        this._negociacoes.adiciona(negociacao);
+
+        // depois de adicionar, atualiza a view novamente para refletir os dados
+        this._negociacoesView.update(this._negociacoes);
     }
 
     
