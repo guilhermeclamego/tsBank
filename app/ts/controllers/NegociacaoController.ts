673a563483c95e686c768ecc000c8740509449c2
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-01-13 10:33:38 -0300

    Melhorias mensagens de execção

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -77,6 +77,9 @@ export class NegociacaoController {
 
                 this._negociacoesView.update(this._negociacoes);
 
+            })
+            .catch(err => {
+                this._mensagemView.update(err.message);
             });
     }
 }
